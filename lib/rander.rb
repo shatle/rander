0d668be57e766731c98fcad2033c84667668c40b
shatle <shatle@qq.com>
2013-08-23 17:34:38 +0800

    updated

--- lib/rander.rb
+++ lib/rander.rb
@@ -8,5 +8,9 @@ module Rander
     origin_arr = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_+%/"
     select_arr = (origin_arr.each_char.to_a - except_arr.take(3)).take(64)
     now_time = Time.now.to_i.to_s(2)
+    num, ext = now_time.length.divmod 6 
+    result_arr = []
+    num2 = num 
+    num.times{ result_arr << select_arr[-6*(num-num2+1),6]; num2=num2 -1 ;}
   end
 end
